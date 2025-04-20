4a4e28bd5b64a62115ad5e062b42b0e98d06ce09
Davidlb365 <davidlb365@gmail.com>
2025-04-20 21:05:51 +0200

    update

--- dist/src/__mocks__/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockResponse = exports.mockRequest = void 0;
-exports.mockRequest = {};
-exports.mockResponse = {
-    send: jest.fn(),
-};
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,5 @@ app.use("/api/auth", authRouter);
 export const server = app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
+
+export default app;
