8082169946a1cbb0291ef1b1317b0d18e8ee4fcd
Davidlb365 <davidlb365@gmail.com>
2025-04-21 11:42:23 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:4200"],
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 
